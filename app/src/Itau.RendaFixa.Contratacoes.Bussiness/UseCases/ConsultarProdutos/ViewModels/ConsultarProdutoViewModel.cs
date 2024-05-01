443429d8c2d13b32b85f4d98cf03fc5bdb88aa80
Carlos Oliveira <carlos.st.oliveira@gmail.com>
2024-04-30 21:13:51 -0300

    Adicionado json property

--- app/src/Itau.RendaFixa.Contratacoes.Bussiness/UseCases/ConsultarProdutos/ViewModels/ConsultarProdutoViewModel.cs
+++ app/src/Itau.RendaFixa.Contratacoes.Bussiness/UseCases/ConsultarProdutos/ViewModels/ConsultarProdutoViewModel.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace Itau.RendaFixa.Contratacoes.Bussiness.UseCases.ConsultarProdutos.ViewModels
 {
     public class ConsultarProdutoViewModel
     {
         [Key]
+        [JsonProperty("id")]
         public int Id { get; set; }
-        public int Id_Tipo_Produto { get; set; }
+        [JsonProperty("Id_Tipo_Produto")]
+        public int IdTipoProduto { get; set; }
+        [JsonProperty("nome")]
         public string? Nome { get; set; }
+        [JsonProperty("bloqueado")]
         public bool Bloqueado { get; set; }
     }
 }
--- app/src/Itau.RendaFixa.Contratacoes.Bussiness/UseCases/ConsultarTipoProdutos/ViewModels/TipoProdutoViewModel.cs
+++ app/src/Itau.RendaFixa.Contratacoes.Bussiness/UseCases/ConsultarTipoProdutos/ViewModels/TipoProdutoViewModel.cs
@@ -1,11 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace Itau.RendaFixa.Contratacoes.Bussiness.UseCases.ConsultarTipoProdutos.ViewModels
 {
     public class TipoProdutoViewModel
     {
         [Key]
+        [JsonProperty("id")]
         public int Id { get; set; }
+        [JsonProperty("nome")]
         public string? Nome { get; set; }
     }
 }
