01e18e2e8f91f6c16576b27640dc20e8e1e77152
Carlos Oliveira <carlos.st.oliveira@gmail.com>
2024-05-08 20:21:47 -0300

    Novas regras de contratações

--- app/src/Itau.RendaFixa.Contratacoes.Bussiness/UseCases/RealizarContratacao/RealizarContratacaoUseCase.cs
+++ app/src/Itau.RendaFixa.Contratacoes.Bussiness/UseCases/RealizarContratacao/RealizarContratacaoUseCase.cs
@@ -18,6 +18,12 @@ namespace Itau.RendaFixa.Contratacoes.Bussiness.UseCases.RealizarContratacao
 
         public async Task<Contratacao> RealizarContratacao(RealizarContratacaoViewModel realizarContratacaoViewModel, CancellationToken cancellationToken = default)
         {
+            if (DiasUteis(DateTime.Parse("2024-05-08")))
+                return null;
+
+            if (!HorarioContratacao())
+                return null;
+
             if (ValidarDesconto(realizarContratacaoViewModel))
                 return null;
 
@@ -35,6 +41,21 @@ namespace Itau.RendaFixa.Contratacoes.Bussiness.UseCases.RealizarContratacao
                 return true;
 
             return false;
+        }
+
+        public bool DiasUteis(DateTime data)
+        {
+            return data.DayOfWeek == DayOfWeek.Saturday || data.DayOfWeek == DayOfWeek.Sunday;
+        }
+
+        public bool HorarioContratacao()
+        {
+            TimeSpan horarioAtual = DateTime.Now.TimeOfDay;
+
+            TimeSpan inicio = new TimeSpan(10, 30, 0);
+            TimeSpan fim = new TimeSpan(22, 0, 0);
+
+            return horarioAtual >= inicio && horarioAtual <= fim;
         }
     }
 }
--- app/src/Itau.RendaFixa.Contratacoes.Bussiness/UseCases/RealizarContratacao/ViewModel/RealizarContratacaoViewModel.cs
+++ app/src/Itau.RendaFixa.Contratacoes.Bussiness/UseCases/RealizarContratacao/ViewModel/RealizarContratacaoViewModel.cs
@@ -6,8 +6,8 @@ namespace Itau.RendaFixa.Contratacoes.Bussiness.UseCases.RealizarContratacao.Vie
     {
         public int IdContratante { get; set; }
         public int IdProduto { get; set; }
-        public DateTime DataOperacao { get; set; }
-        public TimeSpan HoraOperacao { get; set; }
+        public DateTime DataOperacao { get; set; } = DateTime.Now;
+        public TimeSpan HoraOperacao { get; set; } = DateTime.Now.TimeOfDay;
         [Range(0, int.MaxValue, ErrorMessage = "A {0} não pode ser negativa.")]
         public int Quantidade { get; set; }
         [Range(0, int.MaxValue, ErrorMessage = "O {0} não pode ser negativo.")]
